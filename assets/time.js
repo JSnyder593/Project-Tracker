e7491cdcc2d7971edfd2d9071fb93c0741fa9bba
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2022-10-05 11:49:13 -0700

    moment update

---
+++ assets/time.js
@@ -0,0 +1,17 @@
+
+
+// Current date
+var today= moments();
+$("#today").text(today.format("MMM Do, YYYY"));
+// Due date
+var dueDate = ();
+// Days until the due date (use Moment.js to calculate)
+var daysTill = ();
+// Project name
+var projectName = ("");
+// Project type
+var dateSet =();
+// Hourly wage
+var hourlyWag = ();
+// Estimated total earned (hourly wage at 8 hours per day multiplied by the number of days until the due date)
+var wagEarned =();
